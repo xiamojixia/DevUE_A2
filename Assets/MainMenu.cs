33866524c1ed90ae3ace49e84e3e88b6cae37eb7
xiamojixia <2206486730@qq.com>
2025-03-31 16:26:22 +0800

    Initial project, main menu script, page

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("BallGame");
+    }
+
+    public void OpenHelp()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
